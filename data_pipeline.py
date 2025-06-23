634c25862c840ed7438227e9b0116253d64aabff
hxyue1 <hxyue1@gmail.com>
2025-06-23 10:31:47 -0700

    Use black formatter

--- data_pipeline.py
+++ data_pipeline.py
@@ -8,11 +8,12 @@ import pandas as pd
 import torch
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
+
 def read_files(directory, txt_files):
     book_data_list = []
     regex_pattern = re.compile(r"(.*) by (.*).txt")
     for file_name in txt_files:
-        with open(directory + file_name, encoding='utf8') as f:
+        with open(directory + file_name, encoding="utf8") as f:
 
             # Get title and author
             match = re.match(regex_pattern, file_name)
@@ -21,12 +22,13 @@ def read_files(directory, txt_files):
                 authors = match[2]
 
                 book_data = {}
-                book_data['title'] = title
-                book_data['authors'] = authors
-                book_data['text'] = f.read()
+                book_data["title"] = title
+                book_data["authors"] = authors
+                book_data["text"] = f.read()
                 book_data_list.append(book_data)
 
     return book_data_list
+
 
 def document_chunking(document, minimum_chunk_size=None):
     """
@@ -35,34 +37,42 @@ def document_chunking(document, minimum_chunk_size=None):
     """
 
     # Split text by full stop, but add fullstop at the end of each str
-    sentences = document.split('.')
-    sentences = [sentence + '.' for sentence in sentences]
+    sentences = document.split(".")
+    sentences = [sentence + "." for sentence in sentences]
 
     # Accumulate sentences until minimum_chunk_size reached
     chunked_sentences = []
-    chunk = ''
+    chunk = ""
     for sentence in sentences:
         # Add sentence if chunk below threshold
-        if len(chunk) < minimum_chunk_size and len(chunk + sentence) < 2 * minimum_chunk_size:
+        if (
+            len(chunk) < minimum_chunk_size
+            and len(chunk + sentence) < 2 * minimum_chunk_size
+        ):
             chunk += sentence
         # Otherwise store chunk and reset chunk
         else:
             chunked_sentences.append(chunk)
             # Check sentence length
             if len(sentence) > 2 * minimum_chunk_size:
-                sentence_split = [sentence[i:i+minimum_chunk_size] for i in range(0, len(sentence), 2 * minimum_chunk_size)]
+                sentence_split = [
+                    sentence[i : i + minimum_chunk_size]
+                    for i in range(0, len(sentence), 2 * minimum_chunk_size)
+                ]
                 chunked_sentences.extend(sentence_split)
-            chunk = ''
+            chunk = ""
 
-    return chunked_sentences 
+    return chunked_sentences
+
 
 def convert_to_dataset(data_dict):
     """Convert dictionary to hf dataset"""
     data_dict_df = pd.DataFrame(data_dict)
-    data_dict_df = data_dict_df.drop('text', axis=1)
-    data_dict_df = data_dict_df.fillna(method='ffill')
+    data_dict_df = data_dict_df.drop("text", axis=1)
+    data_dict_df = data_dict_df.fillna(method="ffill")
     data_hf = datasets.Dataset.from_pandas(data_dict_df)
     return data_hf
+
 
 def tokenizer_len(text):
     tokens = tokenizer.encode(
@@ -70,8 +80,9 @@ def tokenizer_len(text):
     )
     return len(tokens)
 
+
 def generate_embeddings(model, tokenizer, hf_dataset):
-    
+
     def get_embeddings(text_list):
         encoded_input = tokenizer(
             text_list, padding=True, truncation=True, return_tensors="pt"
@@ -81,21 +92,21 @@ def generate_embeddings(model, tokenizer, hf_dataset):
         embeddings = model_output.last_hidden_state[:, 0]
         return torch.nn.functional.normalize(embeddings, p=2, dim=1)
 
-
     with torch.no_grad():
         embeddings_dataset = hf_dataset.map(
-            lambda x: {'embeddings': get_embeddings(x['chunked']).cpu().numpy()[0]}
+            lambda x: {"embeddings": get_embeddings(x["chunked"]).cpu().numpy()[0]}
         )
 
     return embeddings_dataset
 
-if __name__ == '__main__':
-    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-    model_ckpt = 'BAAI/bge-large-en-v1.5'
+
+if __name__ == "__main__":
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    model_ckpt = "BAAI/bge-large-en-v1.5"
     # model_ckpt = 'thenlper/gte-large'
     tokenizer = AutoTokenizer.from_pretrained(model_ckpt, max_length=512)
     model = AutoModel.from_pretrained(model_ckpt, max_length=512).to(device)
-    directory = 'c:/Users/hxyue/Documents/python-projects/LLM/data/raw/'
+    directory = "c:/Users/hxyue/Documents/python-projects/LLM/data/raw/"
 
     txt_files = []
     for file in os.listdir(directory):
@@ -104,18 +115,18 @@ if __name__ == '__main__':
 
     book_data_list = read_files(directory, txt_files)
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size = 2000,
-        chunk_overlap  = 50,
+        chunk_size=2000,
+        chunk_overlap=50,
         # length_function = tokenizer_len,
-        length_function = len,
-        is_separator_regex = False,
+        length_function=len,
+        is_separator_regex=False,
     )
 
     # Convert to hf dataset
     hf_ds_list = []
     for book_data in book_data_list:
-        print(book_data['title'])
-        book_data['chunked'] = text_splitter.split_text(book_data['text'])
+        print(book_data["title"])
+        book_data["chunked"] = text_splitter.split_text(book_data["text"])
         hf_ds_list.append(convert_to_dataset(book_data))
 
     ds_with_embeddings = []
@@ -123,7 +134,6 @@ if __name__ == '__main__':
         ds_with_embeddings.append(generate_embeddings(model, tokenizer, ds))
 
     dataset_full = datasets.concatenate_datasets(ds_with_embeddings)
-    dataset_full.save_to_disk(directory + 'theology_qa')
-    dataset_full.push_to_hub('hxyue1/theology_qa')
-    huggingface_hub.create_tag('hxyue1/theology_qa', tag='0.1', repo_type='dataset')
-
+    dataset_full.save_to_disk(directory + "theology_qa")
+    dataset_full.push_to_hub("hxyue1/theology_qa")
+    huggingface_hub.create_tag("hxyue1/theology_qa", tag="0.1", repo_type="dataset")
